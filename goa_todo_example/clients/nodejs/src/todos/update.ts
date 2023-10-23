0575c1c40fab36ab13a5f713bf75a38ef8cab098
akm <akm2000@gmail.com>
2023-10-24 08:36:39 +0900

    :factory: $ gonew github.com/akm/goa_todo_example

---
+++ goa_todo_example/clients/nodejs/src/todos/update.ts
@@ -0,0 +1,29 @@
+/**
+ * Usage
+ *   $ npx ts-node src/todos/show.ts <id> <title> open/closed
+ */
+
+import { credentials } from "@grpc/grpc-js";
+
+import {
+  TodosClient,
+  ITodosClient,
+} from "../protos/goagen_goa_todo_example_todos_grpc_pb";
+import {
+  UpdateRequest,
+  UpdateResponse,
+} from "../protos/goagen_goa_todo_example_todos_pb";
+
+const client: ITodosClient = new TodosClient(
+  `localhost:8080`,
+  credentials.createInsecure()
+);
+
+const request: UpdateRequest = new UpdateRequest();
+request.setId(parseInt(process.argv[2], 10));
+request.setTitle(process.argv[3]);
+request.setState(process.argv[4]);
+
+client.update(request, (err: any, response: UpdateResponse) => {
+  console.log(response.toObject());
+});
