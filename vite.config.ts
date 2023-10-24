0426b4c1688adb6b347a7cbbae657169328089b9
akm <akm2000@gmail.com>
2023-10-25 08:14:34 +0900

    :wrench: Remove optimizeDeps.entries and server.hmr.overlay from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		entries: []
-	},
-	server: {
-		hmr: {
-			overlay: false
-		}
-	}
+	plugins: [sveltekit()]
 });
