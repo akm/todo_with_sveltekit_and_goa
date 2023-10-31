143d1dacb51d16e7b5bb2fb2b95109dcbfe209b2
akm <akm2000@gmail.com>
2023-10-31 21:56:03 +0900

    :sparkles: Add grpc_client.ts

--- src/lib/server/grpc_client.ts
+++ src/lib/server/grpc_client.ts
@@ -1,62 +1,49 @@
-import { promisify } from 'util';
-import { credentials } from '@grpc/grpc-js';
+import { ChannelCredentials } from '@grpc/grpc-js';
+import { GrpcTransport } from '@protobuf-ts/grpc-transport';
 
 import type { Todo } from '$lib/models/todo';
-import { TodosClient, type ITodosClient } from './goagen_goa_todo_example_todos_grpc_pb';
-import {
-	ListRequest,
-	ListResponse,
-	CreateRequest,
-	CreateResponse,
-	UpdateRequest,
-	UpdateResponse,
-	DeleteRequest,
-	DeleteResponse
-} from './goagen_goa_todo_example_todos_pb';
+import { TodosClient, type ITodosClient } from './goagen_goa_todo_example_todos.client';
 
-const client: ITodosClient = new TodosClient(`localhost:8080`, credentials.createInsecure());
+const transport = new GrpcTransport({
+	host: 'localhost:8080',
+	channelCredentials: ChannelCredentials.createInsecure()
+});
+
+const client: ITodosClient = new TodosClient(transport);
 
 export const getTodos = async (): Promise<Todo[]> => {
-	const request = new ListRequest();
-	const response = await promisify<ListRequest, ListResponse>(client.list).bind(client)(request);
-
+	const { response } = await client.list({});
 	return (
-		response
-			.getItems()
-			?.getFieldList()
-			.map((item) => {
-				return {
-					id: item.getId().toString(),
-					description: item.getTitle(),
-					done: item.getState() === 'closed'
-				};
-			}) ?? []
+		response.items?.field.map((item) => {
+			return {
+				id: item.id.toString(),
+				description: item.title,
+				done: item.state === 'closed'
+			};
+		}) ?? []
 	);
 };
 
 export const createTodo = async (arg: { description: string }): Promise<{ id: string }> => {
 	const { description } = arg;
-
-	const request = new CreateRequest();
-	request.setTitle(description);
-
-	const response = await promisify<CreateRequest, CreateResponse>(client.create).bind(client)(
-		request
-	);
-	return { id: response.getId().toString() };
+	const { response } = await client.create({
+		title: description,
+		state: 'open'
+	});
+	return { id: response.id.toString() };
 };
 
 export const toggleTodo = async (arg: { id: string | undefined; done: boolean }): Promise<void> => {
 	if (!arg.id) return;
-	const request = new UpdateRequest();
-	request.setId(parseInt(arg.id));
-	request.setState(arg.done ? 'closed' : 'open');
-	await promisify<UpdateRequest, UpdateResponse>(client.update).bind(client)(request);
+	const { response } = await client.show({ id: BigInt(arg.id) });
+	await client.update({
+		id: BigInt(arg.id),
+		title: response.title,
+		state: arg.done ? 'closed' : 'open'
+	});
 };
 
 export const deleteTodo = async (arg: { id: string | undefined }): Promise<void> => {
 	if (!arg.id) return;
-	const request = new DeleteRequest();
-	request.setId(parseInt(arg.id));
-	await promisify<DeleteRequest, DeleteResponse>(client.delete).bind(client)(request);
+	await client.delete({ id: BigInt(arg.id) });
 };
